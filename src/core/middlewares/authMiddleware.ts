6173456ac6cae47733f78aeeac76cee7cd640c14
HsunJ_Local <gkstjswo122@gmail.com>
2025-10-23 15:26:14 +0900

    fix/authMiddleware 에러 수정

--- src/core/middlewares/authMiddleware.ts
+++ src/core/middlewares/authMiddleware.ts
@@ -1,10 +1,10 @@
 import type { RequestHandler } from 'express';
 import { verifyAccessToken } from '#modules/auth/utils/tokenUtils';
 import ApiError from '#errors/ApiError';
-import { User, UserRole } from '@prisma/client';
+import { UserRole } from '@prisma/client';
 
 const authMiddleware: RequestHandler = (req, res, next) => {
-  const token = req.cookies['access_token'];
+  const token = req.cookies['access_token'] as string | undefined;
 
   if (!token) throw new ApiError(401, '로그인이 필요합니다.');
 
