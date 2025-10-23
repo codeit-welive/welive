2e44d610e2acf970497e7f708022cdc0ce507a7c
HsunJ_Local <gkstjswo122@gmail.com>
2025-10-23 14:08:50 +0900

    feat/인증 미들웨어 구현

--- src/core/middlewares/authMiddleware.ts
+++ src/core/middlewares/authMiddleware.ts
@@ -1,16 +1,19 @@
 import type { RequestHandler } from 'express';
 import { verifyAccessToken } from '#modules/auth/utils/tokenUtils';
 import ApiError from '#errors/ApiError';
+import { User, UserRole } from '@prisma/client';
 
 const authMiddleware: RequestHandler = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader?.startsWith('Bearer ') ? authHeader.split(' ')[1] : null;
+  const token = req.cookies['access_token'];
 
   if (!token) throw new ApiError(401, '로그인이 필요합니다.');
 
-  const decoded = verifyAccessToken(token);
+  const decoded = verifyAccessToken(token) as { id: string, role: UserRole };
 
-  req.user = { id: decoded.id };
+  req.user = { 
+    id: decoded.id,
+    role: decoded.role 
+  };
 
   next();
 };
