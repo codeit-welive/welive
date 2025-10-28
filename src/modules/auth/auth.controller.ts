4bf86c6b508d19d4d85ec5b95c00265a1e5dc758
HsunJ_Local <gkstjswo122@gmail.com>
2025-10-28 17:37:25 +0900

    fix: prisma 에러 매핑 위치 변경

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,8 @@
 import { RequestHandler } from 'express';
 import { registSuperAdmin, registAdmin, registUser } from './auth.service';
+import ApiError from '#errors/ApiError';
+import { Prisma } from '@prisma/client';
+import { checkDuplicateData } from './utils/checkDuplicateData';
 
 export const registSuperAdminHandler: RequestHandler = async (req, res, next) => {
   try {
@@ -8,6 +11,10 @@ export const registSuperAdminHandler: RequestHandler = async (req, res, next) =>
     const result = await registSuperAdmin(data);
     res.status(201).json(result);
   } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const errorFields = err.meta?.target as string[];
+      throw ApiError.conflict(checkDuplicateData(errorFields).message);
+    }
     next(err);
   }
 };
@@ -19,6 +26,10 @@ export const registerAdminHandler: RequestHandler = async (req, res, next) => {
     const result = await registAdmin(data);
     res.status(201).json(result);
   } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const errorFields = err.meta?.target as string[];
+      throw ApiError.conflict(checkDuplicateData(errorFields).message);
+    }
     next(err);
   }
 };
@@ -30,6 +41,14 @@ export const registerUserHandler: RequestHandler = async (req, res, next) => {
     const result = await registUser(data);
     res.status(201).json(result);
   } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === 'P2002') {
+        const errorFields = err.meta?.target as string[];
+        throw ApiError.conflict(checkDuplicateData(errorFields).message);
+      } else if (err.code === 'P2025') {
+        throw ApiError.notFound('해당 아파트가 존재하지 않습니다.');
+      }
+    }
     next(err);
   }
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,58 +2,24 @@ import { createSuperAdmin, createAdmin, createUser } from './auth.repo';
 import { SignupSuperAdminRequestDto, SignupAdminRequestDto, SignupUserRequestDto } from './dto/register.dto';
 import { hashPassword } from '#core/utils/passwordUtils';
 import { adminDataMapper } from './utils/dataMapper';
-import { checkDuplicateData } from './utils/checkDuplicateData';
-import ApiError from '#errors/ApiError';
-import { Prisma } from '@prisma/client';
 
 export const registSuperAdmin = async (data: SignupSuperAdminRequestDto) => {
-  try {
-    const createdSuperAdmin = await createSuperAdmin({
-      ...data,
-      password: await hashPassword(data.password),
-    });
+  const createdSuperAdmin = await createSuperAdmin({
+    ...data,
+    password: await hashPassword(data.password),
+  });
 
-    return createdSuperAdmin;
-  } catch (err) {
-    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
-      const errorFields = err.meta?.target as string[];
-      throw ApiError.conflict(checkDuplicateData(errorFields).message);
-    } else {
-      throw err;
-    }
-  }
+  return createdSuperAdmin;
 };
 
 export const registAdmin = async (data: SignupAdminRequestDto) => {
-  try {
-    const { userData, apartmentData } = await adminDataMapper(data);
-    const createdUser = await createAdmin(userData, apartmentData);
+  const { userData, apartmentData } = await adminDataMapper(data);
+  const createdUser = await createAdmin(userData, apartmentData);
 
-    return createdUser;
-  } catch (err) {
-    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
-      const errorFields = err.meta?.target as string[];
-      throw ApiError.conflict(checkDuplicateData(errorFields).message);
-    } else {
-      throw err;
-    }
-  }
+  return createdUser;
 };
 
 export const registUser = async (data: SignupUserRequestDto) => {
-  try {
-    const createdUser = await createUser(data);
-    return createdUser;
-  } catch (err) {
-    if (err instanceof Prisma.PrismaClientKnownRequestError) {
-      if (err.code === 'P2002') {
-        const errorFields = err.meta?.target as string[];
-        throw ApiError.conflict(checkDuplicateData(errorFields).message);
-      } else if (err.code === 'P2025') {
-        throw ApiError.notFound('해당 아파트가 존재하지 않습니다.');
-      }
-    } else {
-      throw err;
-    }
-  }
+  const createdUser = await createUser(data);
+  return createdUser;
 };
