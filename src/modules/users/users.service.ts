fc49bd5c453c73818ea2957551e02e77b91394a2
selentia <admin@duna.me>
2025-11-04 18:35:17 +0900

    feat(users): 404 문구 수정 및 아바타 삭제 처리 추가

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,6 +5,7 @@ import { USER_MESSAGES as MSG } from '#constants/user.constants';
 import { assertAllowedByMagic } from '#core/files/assertAllowedByMagic';
 import { processImageBeforeUpload } from '#core/files/processImageBeforeUpload';
 import { uploadImageToS3 } from '#core/aws/uploadImageToS3';
+import { deleteImageFromS3 } from '#core/aws/deleteImageFromS3';
 
 interface UpdateUserPayload {
   body: {
@@ -18,12 +19,11 @@ interface UpdateUserPayload {
 
 export const updateUserService = async (userId: string, { body, file }: UpdateUserPayload) => {
   const updates: Record<string, any> = {};
+  const user = await findUserById(userId);
+  if (!user) throw ApiError.badRequest(MSG.USER_NOT_FOUND);
 
   // 비밀번호 변경
   if (body.currentPassword && body.newPassword) {
-    const user = await findUserById(userId);
-    if (!user) throw ApiError.badRequest(MSG.UPDATE_FAILED);
-
     const match = await isPasswordValid(body.currentPassword, user.password);
     if (!match) throw ApiError.forbidden(MSG.FORBIDDEN);
 
@@ -51,6 +51,9 @@ export const updateUserService = async (userId: string, { body, file }: UpdateUs
 
   // DB 업데이트
   const updatedUser = await updateUser(userId, updates);
+
+  // 기존 아바타 삭제
+  if (updates.avatar && user.avatar) deleteImageFromS3(user.avatar); // fire-and-forget
 
   return {
     message: `${updatedUser.name}${MSG.UPDATE_SUCCESS}`,
