e9b8a338ecb86193fed505582daf0a914a3e6905
selentia <admin@duna.me>
2025-11-10 11:05:24 +0900

    fix(users): PATCH /api/users/me 파일 업로드 처리 개선

--- src/modules/users/users.router.ts
+++ src/modules/users/users.router.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { validateUserUpdate } from './users.validator';
 import { updateUserController } from './users.controller';
 import authMiddleware from '#core/middlewares/authMiddleware';
 
 const router = Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 /**
  * PATCH /api/users/me
  */
-router.patch('/me', authMiddleware, validateUserUpdate, updateUserController);
+router.patch('/me', authMiddleware, upload.single('file'), validateUserUpdate, updateUserController);
 
 export default router;
