945f248af180abdaad80d53620ea43b9bde712ad
luciakim22 <luciakim22@naver.com>
2025-11-14 15:22:56 +0900

    feat: 투표하기/투표취소 validator 추가

--- src/modules/polls/options/options.router.ts
+++ src/modules/polls/options/options.router.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import requireRole from '#core/middlewares/requireRole';
 import authMiddleware from '#core/middlewares/authMiddleware';
 import { cancelVote, postVote } from './options.controller';
+import { validateDeleteVoteParams, validatePostVoteParams } from './options.validator';
 
 const optionRouter = express.Router();
 
 optionRouter
   .route('/:optionId/vote')
-  .post(authMiddleware, requireRole(['USER']), postVote)
-  .delete(authMiddleware, requireRole(['USER']), cancelVote);
+  .post(authMiddleware, validatePostVoteParams, requireRole(['USER']), postVote)
+  .delete(authMiddleware, validateDeleteVoteParams, requireRole(['USER']), cancelVote);
 
 export default optionRouter;
