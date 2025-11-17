f9f9e061561150ee38d775db2c63257c18c05df8
HsunJ_Local <gkstjswo122@gmail.com>
2025-11-17 17:10:38 +0900

    chore: 검사 순서 수정

--- src/modules/residents/dto/csv.dto.ts
+++ src/modules/residents/dto/csv.dto.ts
@@ -4,8 +4,8 @@ import { z } from 'zod';
 export const residentCsvSchema = z.object({
   building: z.string().nonempty({ message: '동은 필수 입력 항목입니다.' }),
   unitNumber: z.string().nonempty({ message: '호수는 필수 입력 항목입니다.' }),
-  contact: z.string().nonempty({ message: '연락처는 필수 입력 항목입니다.' }),
   name: z.string().nonempty({ message: '이름은 필수 입력 항목입니다.' }),
+  contact: z.string().nonempty({ message: '연락처는 필수 입력 항목입니다.' }),
   isHouseholder: z.enum(IsHouseholder, { message: '세대주여부는 필수 입력 항목입니다.' }),
 });
 
